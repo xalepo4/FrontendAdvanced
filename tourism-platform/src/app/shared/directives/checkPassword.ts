acfc30aae7757e8eea821d38c57cd851fd08edc5
Xavi Ledesma <xalepo4@gmail.com>
2021-03-05 20:28:46 +0100

    Add type in password check

--- tourism-platform/src/app/shared/directives/checkPassword.ts
+++ tourism-platform/src/app/shared/directives/checkPassword.ts
@@ -1,8 +1,7 @@
 import {FormGroup} from '@angular/forms';
 
 export class CheckPassword {
-  // tslint:disable-next-line:typedef
-  public static checkInvalidPassword(frm: FormGroup) {
+  public static checkInvalidPassword(frm: FormGroup): { mismatch: boolean } {
     return frm.controls.password.value === frm.controls.confirmPassword.value ? null : {mismatch: true};
   }
 }
