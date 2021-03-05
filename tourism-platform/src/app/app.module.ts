28aa128b81062a92e4b63e5b39ed0724443b5a1e
Xavi Ledesma <xalepo4@gmail.com>
2021-03-05 16:56:26 +0100

    Add header and first components

---
+++ tourism-platform/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ActivityListComponent } from './views/activity-list/activity-list.component';
+import { ActivityDetailComponent } from './views/activity-detail/activity-detail.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ActivityListComponent,
+    ActivityDetailComponent,
+    ProfileComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
