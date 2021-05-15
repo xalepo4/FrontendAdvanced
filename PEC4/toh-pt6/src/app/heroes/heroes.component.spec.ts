0b75ca3d88f14a3b1e68a257091d6b24996b1748
Xavi Ledesma <xalepo4@gmail.com>
2021-05-15 14:51:29 +0200

    Update exercise comment

--- PEC4/toh-pt6/src/app/heroes/heroes.component.spec.ts
+++ PEC4/toh-pt6/src/app/heroes/heroes.component.spec.ts
@@ -3,6 +3,8 @@ import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {HeroService} from '../hero.service';
 
+// Exercise 5
+
 describe('HeroesComponent', () => {
   let component: HeroesComponent;
   let fixture: ComponentFixture<HeroesComponent>;
@@ -23,7 +25,6 @@ describe('HeroesComponent', () => {
     fixture.detectChanges();
   });
 
-  // Exercise 5
   it('exists h2 with My Heroes text', () => {
     const h2Element: HTMLElement = fixture.nativeElement;
     const h2 = h2Element.querySelector('h2');
@@ -37,11 +38,6 @@ describe('HeroesComponent', () => {
     addButton.click();
     expect(component.add).toHaveBeenCalled();
   });
-
-  // Exercise 6
-
-
-
 });
 
 
